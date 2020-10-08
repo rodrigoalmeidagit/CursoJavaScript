54fc46404fa4cbed3a5df8fc2891624b17d496c6
novators3k <novators@gmail.com>
2020-10-08 00:33:18 -0300

    update js
    atualizado até aula 11
    condicional simples.

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 70) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+} else {
+    console.log('Dirija sempre usando o cinto de segurança!')
+}
+
+
