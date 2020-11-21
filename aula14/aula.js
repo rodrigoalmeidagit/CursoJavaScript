1901babd7c1ffb327098c2e3b008a015d9ab864b
novators3k <novators@gmail.com>
2020-11-21 12:44:25 -0300

    create aula14
    estrutura FOR

---
+++ aula14/aula.js
@@ -0,0 +1,22 @@
+/* Estrutura FOR 
+
+for (inicio; teste; incremento ){
+
+}
+
+var c = 5;
+while (c <= 100) {
+  console.log(c)
+  c++
+}
+*/
+
+console.log('Vai começar!!!')
+
+for (var c = 1; c <= 10; c += 1) {
+  console.log(`Passo ${c}`)
+}
+console.log('Fim!!!')
+
+
+
