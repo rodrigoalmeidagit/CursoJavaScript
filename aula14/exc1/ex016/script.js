84a6287a0e82d8a8df2733b8131a1ea67458ee04
novators3k <novators@gmail.com>
2020-11-22 02:43:27 -0300

    update script.js ex016

--- aula14/exc1/ex016/script.js
+++ aula14/exc1/ex016/script.js
@@ -5,15 +5,14 @@ function contar() {
   let res = document.getElementById('res')
 
   if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-    window.alert('ERRO: Valor Inválido!')
-  } else {
-      res.innerHTML = 'Contando: '
-      let i = Number(ini.value)
-      let f = Number(fim.value)
-      let p = Number(passo.value)
+    alert('ERRO: Preencha todos os campos')
+  } else {    
+    res.innerHTML = 'Contando: '
+    let i = Number(ini.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
 
-      for(let c = i; c <= f; c += p) {
-        res.innerHTML += `${c} \u{1F603}`
-      }
-    }
+    for (let c = i; c <= f; c += p)
+    res.innerHTML += `${c}`
+  }
 }
